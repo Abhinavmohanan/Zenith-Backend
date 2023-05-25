48dc6436c2de2e07af80032b6652e1e095f2e6a7
Abhinav <abhinavmohanan17@gmail.com>
2023-05-25 16:20:39 +0530

    Changes

--- routes/loginRoute.js
+++ routes/loginRoute.js
@@ -27,7 +27,7 @@ loginRoute.post('/',async (req,res)=>{
     }
     const refreshToken = getResfreshToken({email:user.email});
 
-    res.cookie("refreshToken",refreshToken,{httpOnly:true,expires:new Date(Date.now()+7*24*60*60*1000),sameSite: 'none',secure:true,domain:process.env.FRONT_END_URL})
+    res.cookie("refreshToken",refreshToken,{httpOnly:true,expires:new Date(Date.now()+7*24*60*60*1000),sameSite: 'none',secure:true,domain:process.env.COOKIE_DOMAIN})
 
     user.refreshToken = refreshToken;
     try{
